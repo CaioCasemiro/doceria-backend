c431c106bb6ae72de5588ae911fb9c18fc751d8d
CaioCasemiro <casemirocaio2@gmail.com>
2025-11-08 17:57:24 -0300

    ajuste no cors

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ import adminRouter from "./routes/admin.js";
 
 const app = express();
 
-app.use(cors()); 
+app.use(cors({
+  origin: "https://adocicadadoceria.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json()); 
 
 app.use("/pedidos", pedidosRouter);
